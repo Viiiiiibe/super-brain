f60d60147d8197c56c1f1747f2a5ec7f4504598e
Viiiiiibe <igoryur4@yandex.ru>
2024-10-23 19:59:18 +0300

    добавил urls и views для фронтендера в courses

--- proj/courses/urls.py
+++ proj/courses/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('index/', views.index, name='index'),
+    path('all_courses/', views.all_courses, name='all_courses'),
+    path('all_courses/<slug:slug>/', views.category_courses, name='category_courses'),
+    path('course/<slug:slug>/', views.course, name='course'),
+    path('problem/<int:problem_id>/', views.problem, name='problem'),
+    path('tournament/<int:tournament_id>/', views.tournament, name='tournament'),
+    path('tournament_problem/<int:tournament_problem_id>/', views.tournament_problem, name='tournament_problem'),
 ]
--- proj/courses/views.py
+++ proj/courses/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'courses/index.html')
+
+
+def all_courses(request):
+    return render(request, 'courses/all_courses.html')
+
+
+def category_courses(request, slug):
+    return render(request, 'courses/category_courses.html')
+
+
+def course(request, slug):
+    return render(request, 'courses/course.html')
+
+
+def problem(request, problem_id):
+    return render(request, 'courses/problem.html')
+
+
+def tournament(request, tournament_id):
+    return render(request, 'courses/tournament.html')
+
+
+def tournament_problem(request, tournament_problem_id):
+    return render(request, 'courses/tournament_problem.html')
