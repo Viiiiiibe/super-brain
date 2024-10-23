15bee6e859cd27b54372921e2fb81bb76fe549d8
Viiiiiibe <igoryur4@yandex.ru>
2024-10-23 20:43:23 +0300

    добавил urls и views для фронтендера в account

---
+++ proj/account/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
+    path('login/', views.login, name='login'),
+
+    path('password_change/', views.password_change, name='password_change'),
+    path('password-change/done/', views.password_change_done, name='password_change_done'),
+    path('password_reset/', views.password_reset, name='password_reset'),
+    path('password_reset/done/', views.password_reset_done, name='password_reset_done'),
+    path('reset/uidb64/token/', views.password_reset_confirm, name='password_reset_confirm'),
+    path('reset/done/', views.password_reset_complete, name='password_reset_complete'),
+
+    path('personal_account_main/', views.personal_account_main, name='personal_account_main'),
+    path('order_an_individual_course/', views.order_an_individual_course, name='order_an_individual_course'),
+    path('order_an_individual_course/done/', views.order_an_individual_course_done,
+         name='order_an_individual_course_done'),
+]
--- proj/account/views.py
+++ proj/account/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request):
+    return render(request, 'account/signup.html')
+
+
+def logout(request):
+    return render(request, 'account/logout.html')
+
+
+def login(request):
+    return render(request, 'account/login.html')
+
+
+def password_change(request):
+    return render(request, 'account/password_change_form.html')
+
+
+def password_change_done(request):
+    return render(request, 'account/password_change_done.html')
+
+
+def password_reset(request):
+    return render(request, 'account/password_reset_form.html')
+
+
+def password_reset_done(request):
+    return render(request, 'account/password_reset_done.html')
+
+
+def password_reset_confirm(request):
+    return render(request, 'account/password_reset_confirm.html')
+
+
+def password_reset_complete(request):
+    return render(request, 'account/password_reset_complete.html')
+
+
+def personal_account_main(request):
+    return render(request, 'account/personal_account_main.html')
+
+
+def order_an_individual_course(request):
+    return render(request, 'account/order_an_individual_course.html')
+
+
+def order_an_individual_course_done(request):
+    return render(request, 'account/order_an_individual_course_done.html')
