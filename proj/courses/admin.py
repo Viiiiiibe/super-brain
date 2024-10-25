2e7692aad034c212e8eed83064faaff35a06f7ca
Viiiiiibe <igoryur4@yandex.ru>
2024-10-25 18:31:34 +0300

    Написал модели приложения courses

--- proj/courses/admin.py
+++ proj/courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Course, Problem, PersonalCourse, PersonalProblem, Tournament, TournamentProblem
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Problem)
+admin.site.register(PersonalCourse)
+admin.site.register(PersonalProblem)
+admin.site.register(Tournament)
+admin.site.register(TournamentProblem)
